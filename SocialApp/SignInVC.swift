a7d454dbc9b77bd6574c88e1fc6bc54d5fcac3fd
dat.nguyen <datnguyen231@gmail.com>
2017-02-17 16:32:56 +0700

    Finish login with FB

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -1,5 +1,5 @@
 //
-//  
+//
 
 //  SocialApp
 //
@@ -8,6 +8,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -16,11 +19,36 @@ class SignInVC: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func fbSignIn_OnPressed(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler: { (result, error) in
+            if error != nil {
+                print("FUCK: Unable to authenticate with Facebook")
+            } else if result?.isCancelled == true {
+                print("OHHHH: User cancelled authenticate with Facebook")
+
+            } else {
+                print("JESS: Successfully authenticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        })
+
     }
 
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+
+            if error != nil {
+                print("FUCK: Unable to authenticate with Firebase - \(String(describing: error))")
+
+            } else {
+                print("JESS: Successfully authenticated with Firebase")
+
+            }
+
+        })
+    }
 
 }
 
